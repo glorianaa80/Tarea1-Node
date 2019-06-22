68a295f159c10b674dfb472a90eab411ccaa10ed
Gloriana Angulo Alvarado <g.angulo@b7-09.local>
2019-06-22 17:28:02 -0600

    report

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const http = require('http');
 
 http.createServer(f).listen(5000);
 
-let JsonCars = require('./cars.json')
+let JsonCars = require('./cars.json');
+let JsonBrands = require('./brands.json')
 
 
 function f(req, res) {
@@ -15,9 +16,31 @@ function f(req, res) {
         data:JsonCars 
       }));
     }
-  } else  res.end(JSON.stringify({
+  }
+  
+  if (req.url === '/api/v1/brands') {
+    if (req.method === 'GET') {
+      res.end(JSON.stringify({
+        success:true,
+        error:null,
+        data:JsonBrands 
+      }));
+    }
+  } 
+
+  if (req.url === '/api/v1/cars/report') {
+    let h = Object.keys(JsonCars[0]).join(',')+ "\n";
+    let body = '';
+    JsonCars.forEach(o => {
+      body += Object.keys(0).map(k => o[k]).join(',')+ "\n"
+    }); 
+    h += body;
+    res.end(h);
+  }
+
+  else  res.end(JSON.stringify({
         success:false,
         error:'error',
         data:null 
-  }))
+  }));
 }
